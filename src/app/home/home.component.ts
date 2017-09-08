5917f82ca976ae6b27dbcda88e319e6ba2dfd955
Ramy Hakam <pencilsoft1@gmail.com>
2017-09-09 01:36:05 +0200

    Adding Custom views

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,15 +9,19 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
 CountMyMessages : number= 0;
 CountServerMessages:number=0;
+ChatArray :Array<string>=[];
 
-
-  constructor( private myWS : RealTimeService ) {
+constructor( private myWS : RealTimeService ) {
 this.myWS.getFromEvent().subscribe(
-  result=>{console.log(result)
+  result=>{console.log(result);
+
+
  this.CountServerMessages+=1; 
   let sound =new Audio("assets/dist/sound/User.wav");
 sound.play();  
-  },
+this.ChatArray.push("Admin");
+
+},
   error=>{console.log(error)});
   
 }
@@ -28,8 +32,10 @@ sound.play();
   ngOnInit() {
     setInterval(()=>{this.myWS.SendME("What is Up Server !!");
   this.CountMyMessages+=1;
+    this.ChatArray.push("User");
+
   },6000);
-    
+  
   }
 
 }
