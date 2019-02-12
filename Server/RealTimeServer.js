c4ee12c952bafb81d0e0e05acee6636775fdb9c0
Ramy Hakam <pencilsoft1@gmail.com>
2019-02-12 11:06:10 +0200

    update socket server to emit on every call

--- Server/RealTimeServer.js
+++ Server/RealTimeServer.js
@@ -20,31 +20,33 @@ var io= socketio(server);
 io.on('connection',(socket)=> {
 console.log("New User conections");
 //on disConnections 
-
+//on send event from the user 
 socket.on('SEND',function(Message){
+    console.log('send from user ',Message);
 
-    player.play('server.wav', function(err){
-        if (err) throw err
-      })
-    console.log(Message);
-    });
+    socket.emit('HiThere',createServerMessage());
+    console.log("send from server",createServerMessage());
+});
 
 //Send Welcome Message To the user 
 
-setInterval(()=>{
-socket.emit('HiThere','Welocme To My Server Bro');
-console.log("send from server");
-},7000);
+// setInterval(()=>{
 
-//Get Welcome Message From The User 
+// socket.emit('HiThere','Welocme To My Server Bro');
+// console.log("send from server");
+// },7000);
 
-});
+// //Get Welcome Message From The User 
 
-
-
+ });
 
 
 
+
+//create message function 
+function createServerMessage() {
+ return {'user':'Admin','message':'Pong from socket server'}
+}
 
 //Launch the Server on port 3000
 
